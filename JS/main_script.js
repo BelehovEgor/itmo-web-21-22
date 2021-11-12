ffa809b20fdad50997769234005c1d71a886f35d
BelehovEgor <belehov.egor2001@yandex.ru>
2021-11-12 22:51:02 +0300

    test

---
+++ JS/main_script.js
@@ -0,0 +1,19 @@
+window.addEventListener('load', () => 
+{
+	var loadTime = window.performance.timing.domContentLoadedEventEnd - window.performance.timing.navigationStart;
+	document.getElementById("task4-2").innerText = `Время: ${loadTime}`;
+});
+
+var local = document.location.href.split('/'); 
+switch (local[local.length - 1]) {
+	case "index.html":
+		document.getElementById("main").classList.add("navigation__tag_selected");
+		break;
+	case "knowledge_page.html":
+		document.getElementById("knowledge").classList.add("navigation__tag_selected");
+		break;
+	case "experience_page.html":
+		document.getElementById("experience").classList.add("navigation__tag_selected");
+		break;
+}
+	
